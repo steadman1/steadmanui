1adf0612a9f5163b6b6cd4ae3978455e223f0128
Spencer <64872573+steadman1@users.noreply.github.com>
2023-07-06 11:25:12 -0400

    Exposed all elements with `public`

--- Sources/SteadmanUI/Extensions.swift
+++ Sources/SteadmanUI/Extensions.swift
@@ -7,18 +7,18 @@
 
 import SwiftUI
 
-enum PastelIntensity: Double {
+public enum PastelIntensity: Double {
     case low = 0.1, mid = 0.2, high = 0.25
 }
 
 extension Color {
-    func pastelLighten(intensity: PastelIntensity = .low, animatedValue: Double = 1) -> Color {
+    public func pastelLighten(intensity: PastelIntensity = .low, animatedValue: Double = 1) -> Color {
         return self.opacity(intensity.rawValue * animatedValue)
     }
 }
 
 extension Array where Element: Equatable {
-    func without(_ elementToRemove: Element) -> [Element] {
+    public func without(_ elementToRemove: Element) -> [Element] {
         var array = self
         for (index, element) in zip(self.indices, self) {
             if element == elementToRemove {
