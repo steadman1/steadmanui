aacce532cbf1ba8442a5a9c0c5f9ff03c4a80db6
Spencer <64872573+steadman1@users.noreply.github.com>
2023-10-11 11:26:02 -0400

    Update NavigationBar.swift

--- Sources/SteadmanUI/NavigationBar.swift
+++ Sources/SteadmanUI/NavigationBar.swift
@@ -35,11 +35,11 @@ public struct NavigationItem: View {
         return HStack {
             if activeIcon != nil && isActive {
                 activeIcon
-                    .font(.miniIcon)
+                    .font(NavigationBar.iconFont)
                     .foregroundColor(foregroundColor)
             } else {
                 icon
-                    .font(.miniIcon)
+                    .font(NavigationBar.iconFont)
                     .foregroundColor(foregroundColor)
             }
             if animation != 0 {
@@ -97,6 +97,7 @@ public class NavigationBar: ObservableObject {
     public static var foregroundItemColor: Color = .blue
     public static var backgroundColor: Color = .white
     public static var cornerRadius: CGFloat = 50
+    public static var iconFont: Font = .miniIcon
     public static var font: Font = .system(size: 14).bold()
     static let height: CGFloat = 116
     static let halfHeight: CGFloat = 58
